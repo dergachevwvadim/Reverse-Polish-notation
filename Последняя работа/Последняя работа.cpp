2092e7ad90571ece4b640b76fd7d9cb41d909644
Вадим Дергачев <vadim.valentinovic@mail.ru>
2022-05-23 15:38:02 +0300

    Почти законченная

--- Последняя работа/Последняя работа.cpp
+++ Последняя работа/Последняя работа.cpp
@@ -1,96 +1,107 @@
 ﻿#include <stdio.h>
 #include <malloc.h>
+#include <math.h>
 
+int size_stack;
 int sp = 0;
-double stack[100];
-int pop(void)
+bool FLAG = 0;
+int pop(int* stack)
+
 {
-    if (sp > 0) 
-    {
-        return stack[--sp];
-    }
-    else 
-    {
-        fprintf(stderr, "Error POP\n");
-        return 0;
-    }
+	if (sp > 0)
+	{
+		return stack[--sp];
+	}
+	else
+	{
+		//	fprintf(stderr, "Error POP\n");
+		//	return 0;
+	}
 }
-void push(int a) 
+void push(int* stack, int a)
 {
-    stack[sp++] = a;
+	stack[sp++] = a;
 }
-int empty() 
+int empty()
 {
-    return (sp == 0);
+	return (sp == 0);
 }
 
-int main() 
+int main()
 {
+	printf("input size of stack\n");
+	scanf_s("%d", &size_stack);
+	int* szst = (int*)malloc(size_stack * sizeof(int));
+	int i;
+	printf("List of commands \n summ(+);\n sub(-); \n multip(*); \n div(/); \n exp(^);\n exit(E) \n");
+	char p;
+	while (1)
+	{
+		p = getchar();
+		if (p == 'E')
+		{
+			printf("Exit\n");
+			break;
+		}
+		if (p >= 48 && p <= 57)
+		{
+			push(szst, p - 48);
+		}
+		int x;
+		switch (p)
+		{
+		case '\n':
+		{
+
+		}
+		case ' ':
+		{
+			break;
+		}
+		case '+':
+		{
+			push(szst, pop(szst) + pop(szst));
+			break;
+		}
+		case '-':
+		{
+			push(szst, -pop(szst) + pop(szst));
+			break;
+		}
+		case '*':
+		{
+			push(szst, pop(szst) * pop(szst));
+			break;
+		}
+		case '/':
+		{
+			push(szst, pop(szst) / pop(szst));
+			break;
+		}
+		case '=':
+		{
+			printf("Result = %d\n", pop(szst));
+			break;
+		}
 
 
-    int i;
-    printf("List of commands \n summ(+);\n sub(-); \n multip(*); \n div(/); \n exp(^);\n sqrt(s);\n exit(E) \n");
-    char p;
-    while (1)
-    {
-        p = getchar();
-        if (p == 'E')
-        {
-            printf("Exit\n");
-            break;
-        }
-        int x;
-        switch (p)
-        {
-        case '\n':
-        {
+		if (FLAG)
+			break;
 
-        }
-        case ' ':
-        {
-            break;
-        }
-        case '+':
-        {
-            push(pop() + pop());
-            break;
-        }
-        case '-':
-        {
-            push(-pop() + pop());
-            break;
-        }
-        case '*':
-        {
-            push(pop() * pop());
-            break;
-        }
-        case '/':
-        {
-            push(pop() / pop());
-            break;
-        }
-        case '=':
-        {
-            printf("Result = %d\n", pop());
-            break;
-        }
-        case 9:
-        {
-            goto RESULT;
-        }
-        default:
-        {
-        }
+		
+		default:
+		{
+			if (p >= 48 && p <= 57)
+				continue;
+			else
+				printf("Error\n");
+		}
+		}
 
-        }
-}
-RESULT:
-    i = 0;
-    while (!empty())
-    {
-        printf("Stack[%d] = %d\n", i, pop());
-        i++;
-    }
-    return 0;
+		if (FLAG)
+		{
+			free(szst);
+			break;
+		}
+	}
 }
